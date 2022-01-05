8268848a5842c53db40980cd78366493feeeb3fc
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-01-05 12:04:39 +0100

    Introduce the target branch
    Introduce $CHANGE_TARGET so the validation use automatically the target branch name

--- scripts/installCMSSWRef.sh
+++ scripts/installCMSSWRef.sh
@@ -13,6 +13,6 @@ echo $PWD
 eval `scramv1 runtime -sh`
 git config --global user.name "Emilia Becheva"
 git config --global user.email emilia.becheva@llr.in2p3.fr
-git cms-merge-topic ebecheva:main-dev-$1
-git checkout -b local_main-dev-$1 ebecheva/main-dev-$1
+git cms-merge-topic ebecheva:$2
+git checkout -b local_$2 ebecheva/$2
 scram b -j8
