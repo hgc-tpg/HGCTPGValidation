87ad2cc3026f26b7936c03dee4a6632950be2205
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-01-05 16:49:28 +0100

    Testing git config
    Testing git config

--- scripts/installCMSSWRef.sh
+++ scripts/installCMSSWRef.sh
@@ -11,8 +11,8 @@ scramv1 p -n $1_HGCalTPGValidation_ref CMSSW $1
 cd $1_HGCalTPGValidation_ref/src
 echo $PWD
 eval `scramv1 runtime -sh`
-git config --global user.name "Emilia Becheva"
-git config --global user.email emilia.becheva@llr.in2p3.fr
+#git config --global user.name "Emilia Becheva"
+#git config --global user.email emilia.becheva@llr.in2p3.fr
 git cms-merge-topic ebecheva:$2
 git checkout -b local_$2 ebecheva/$2
 scram b -j8
