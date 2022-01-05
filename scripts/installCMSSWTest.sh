6809fb459c9ff856e3105d1420e7154338f3a457
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-01-05 12:09:39 +0100

    Add $CHANGE_BRANCH
    Give the name of the change branch

--- scripts/installCMSSWTest.sh
+++ scripts/installCMSSWTest.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 
 # installCMSSWTest.sh $RELEASE
-# $1 name of the release
+# $1 name of the release, it is taken from the $CHANGE_TARGET
+# $2 name of the change branch $CHANGE_BRANCH
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc7_amd64_gcc900
@@ -12,6 +13,6 @@ scramv1 p -n $1_HGCalTPGValidation_test $1
 cd $1_HGCalTPGValidation_test/src
 echo $PWD
 eval `scramv1 runtime -sh`
-git cms-merge-topic ebecheva:fix-dev-$1
-git checkout -b local_main-dev-$1 ebecheva/fix-dev-$1
+git cms-merge-topic ebecheva:$2
+git checkout -b local_$2 ebecheva/$2
 scram b -j8
