6a0f6eedeea949dd43b0ba8c4d3dac6619b5bb62
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-01-17 17:01:48 +0100

    Update installCMSSWTest.sh

--- scripts/installCMSSWTest.sh
+++ scripts/installCMSSWTest.sh
@@ -9,7 +9,7 @@ source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc7_amd64_gcc900
 echo $SCRAM_ARCH
 module purge
-scramv1 p -n $1_HGCalTPGValidation_test $1
+scramv1 p -n $1_HGCalTPGValidation_test CMSSW $1
 cd $1_HGCalTPGValidation_test/src
 echo $PWD
 eval `scramv1 runtime -sh`
