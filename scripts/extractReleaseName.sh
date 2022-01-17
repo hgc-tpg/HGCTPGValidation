7d465b36210d44de406bf5e22aa7efda07f39f94
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2022-01-17 11:35:11 +0100

    Clean debug message

--- scripts/extractReleaseName.sh
+++ scripts/extractReleaseName.sh
@@ -11,10 +11,10 @@ do
     rel=$j
     if [ "$rel" == "CMSSW" ]
     then
-    echo "===The name of the release rel is $s"
+    #echo "===The name of the release rel is $s"
     export REF_RELEASE=$s
     break
   fi
   done
 done
-echo "release after for => $refrel"
+echo "The name of the release is $refrel"
