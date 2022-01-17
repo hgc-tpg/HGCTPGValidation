d19fe37f792c0dcb7948135a12d6f37b1c8e9181
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2022-01-17 23:43:16 +0100

    Add env variable LABEL in produceData.sh script

--- scripts/produceData.sh
+++ scripts/produceData.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # ./produceData.sh
-# $1 release name
-# $2 target branch name
-# $3 label "ref" or "test"
+# $1 label "ref" or "test"
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 echo $PWD
@@ -17,4 +15,4 @@ cmsDriver.py hgcal_tpg_validation -n 50 + \
 --inputCommands "keep *","drop l1tEMTFHit2016Extras_simEmtfDigis_CSC_HLT","drop l1tEMTFHit2016Extras_simEmtfDigis_RPC_HLT","drop l1tEMTFHit2016s_simEmtfDigis__HLT","drop l1tEMTFTrack2016Extras_simEmtfDigis__HLT","drop l1tEMTFTrack2016s_simEmtfDigis__HLT" + \
 --filein file:/data_CMS_upgrade/becheva/data_jenkins/003F2BA9-0D02-5A43-A53F-4161E513BFA6.root + \
 --no_output + \
---customise_commands "process.MessageLogger.files.out = dict(); process.Timing = cms.Service('Timing', summaryOnly = cms.untracked.bool(False), useJobReport = cms.untracked.bool(True)); process.SimpleMemoryCheck = cms.Service('SimpleMemoryCheck', ignoreTotal = cms.untracked.int32(1)); process.schedule = cms.Schedule(process.user_step)"
+--customise_commands "process.MessageLogger.files.out_$1 = dict(); process.Timing = cms.Service('Timing', summaryOnly = cms.untracked.bool(False), useJobReport = cms.untracked.bool(True)); process.SimpleMemoryCheck = cms.Service('SimpleMemoryCheck', ignoreTotal = cms.untracked.int32(1)); process.schedule = cms.Schedule(process.user_step)"
