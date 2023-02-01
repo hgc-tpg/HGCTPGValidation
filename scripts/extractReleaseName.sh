dfa22e99a880d018ae54710d335604c4325bc46a
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2023-02-01 17:33:29 +0100

    Minor changes in order to use REF_RELEASE and SCRAM_ARCH as env var in Jenkinsfile

--- scripts/extractReleaseName.sh
+++ scripts/extractReleaseName.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# source ../HGCTPGValidation/scripts/extractReleaseName.sh $CHANGE_TARGET
+
+# Important
+# When this script is used in Groovy script, it is imperative to not use other echo messages
+# except the last "echo -n "$REF_RELEASE"" message
+# The "-n" option is mandatory in order to out result without a newline 
 
 ########################################################
 IFS="-"
@@ -11,11 +17,12 @@ do
     rel=$j
     if [ "$rel" == "CMSSW" ]
     then
-    echo "===The name of the release rel is $s"
+    #echo "===The name of the release is $s"
     export REF_RELEASE=$s
     break
   fi
   done
 done
-echo "The name of the release is $REF_RELEASE"
 unset IFS
+echo -n "$REF_RELEASE"
+
