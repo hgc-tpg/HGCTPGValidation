56902b01545162662479477ad9c956af5ea30865
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2023-06-12 15:33:59 +0200

    First extractTimeMemoryInfos for test release

--- scripts/displayHistos.py
+++ scripts/displayHistos.py
@@ -245,14 +245,15 @@ def main(configset, refdir, testdir, datadir, prnumber, prtitle):
         if testdir==refdir:
             print("testdir and refdir are the same.")
             print("cp out_" + confTest + "_test.log out_" + confRef + "_ref.log")
+            os.system("ls -lrt")
             os.system("cp out_" + confTest + "_test.log out_" + confRef + "_ref.log")
             os.system("ls -lrt")
         else:
             print("testdir and refdir are different.")
         
-        # Extract Memory Check information and global Time information   
-        extractTimeMemoryInfos("out_" + confRef + "_ref.log", refdir)
+        # Extract Memory Check information and global Time information
         extractTimeMemoryInfos("out_" + confTest + "_test.log", testdir)
+        extractTimeMemoryInfos("out_" + confRef + "_ref.log", refdir)
      
         # Create histograms Time/event/producer from TimingInfo_.txt 
         readFileStatement(confRef, "ref", refdir)
