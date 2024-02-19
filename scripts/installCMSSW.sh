4e3ddabacca6325eab3282c6014d2a898ad77bda
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2024-02-19 15:51:43 +0100

    Add get_status function for checking the status of each commands in the script installCMSSW.sh

--- scripts/installCMSSW.sh
+++ scripts/installCMSSW.sh
@@ -11,6 +11,7 @@
 # $6 reference branch name (this is the target or base branch to which the change could be merged, it is in https://github.com/hgc-tpg repository)
 # $7 label "ref" or "test"
 
+set -x
 export SCRAM_ARCH=$1
 echo $SCRAM_ARCH
 relversion=$2
@@ -26,15 +27,32 @@ echo $branch_ref
 label=$7
 echo $label
 
+# Check the status after executing a command
+get_status(){
+if [ $? -eq 0 ]; 
+then 
+  echo "=== The command finished successfully: " $1
+else 
+  echo "===== > Error: " $1
+  exit 1
+fi
+}
+
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 module purge
+# Install working directory for the release CMSSW $relversion
 scramv1 p -n ${relversion}_HGCalTPGValidation_$label CMSSW $relversion
+get_status
 cd ${relversion}_HGCalTPGValidation_$label/src
-echo $PWD
 eval `scramv1 runtime -sh`
 # Get the reference (target) branch from the base remote
 git cms-merge-topic $baseremote:$branch_ref
+get_status
 git checkout -b local_$branch_ref $baseremote/$branch_ref
+get_status
 # Merge the change branch into the reference branch
 git cms-merge-topic $remote:$branch
+get_status
+# Compile
 scram b -j8
+get_status
