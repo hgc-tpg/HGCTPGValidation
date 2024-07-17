a50856f0fa560611eaa5946f5ab1f1ea8145d286
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2024-07-18 00:58:43 +0200

    Select pid for user=jenkins and process_name=cmsRun, add limit for the rss memory

--- scripts/get_rss_memory.sh
+++ scripts/get_rss_memory.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Usage: call the script in Jenkinsfile immediately after 
-# python ../../../HGCTPGValidation/scripts/produceData_multiconfiguration.py --subsetconfig ${CONFIG_SUBSET} --label ${LABEL_REF}
-# get_rss_memory.sh $! interval
+# Usage: call the script in HGCTPGValidation/scripts/produceData_multiconfiguration.py immediately after cmsRun
+# get_rss_memory.sh $! interval limit
 # 1rst argument: "$!" is the PID of python produceData_multiconfiguration.py process
 # 2nd argument: interval in seconds
+# 3th argument: memory limit
 
 # Check if process name is provided
 if [ -z "$1" ]; then
@@ -13,18 +13,30 @@ if [ -z "$1" ]; then
 fi
 
 # Get the PID of the process
-PID=$1
-echo $PID
 INTERVAL=$2
+RSS_limit=$3
 
 while true; do
-    PID=$1
+    echo "LastProcess PID= " $1
+    p_all=$(ps -eo pid,user,comm | grep cmsRun | grep jenkins | awk '{print}')
+    echo $p_all
+    PID=$(ps -eo pid,user,comm | grep cmsRun | grep jenkins | awk '{print $1}')
+    echo "PID=" $PID
     
     if [ -z "$PID" ]; then
         echo "Process $PID not found."
     else
         # Get the RSS (Resident Set Size) memory usage
+        cp /proc/$PID/status ./status_${PID}
+        ps
+        echo "Get RSS value "
         RSS=$(grep -i vmrss /proc/$PID/status | awk '{print $2}')
+        echo "===> RSS memory ${RSS}"
+        if [ "${RSS}" -gt "${RSS_limit}" ]; then
+            kill -9 $PID;
+            echo "===> RSS memory ${RSS} > RSS limit ${RSS_limit}";
+            break;
+        fi
         
         # Output the RSS memory usage
         if [ -e /proc/$PID/status ]; then
