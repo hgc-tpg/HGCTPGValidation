a6d025082b357b93680ca759245ad88a2734295f
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2024-07-18 17:49:56 +0200

    Combine two if statement, add printing messages

--- scripts/get_rss_memory.sh
+++ scripts/get_rss_memory.sh
@@ -6,9 +6,21 @@
 # 2nd argument: interval in seconds
 # 3th argument: memory limit
 
-# Check if process name is provided
+# Check if the PID of the last process is provided
 if [ -z "$1" ]; then
-    echo "Usage: $0 PID"
+    echo "Usage: $0 PID INTERVALL RSS_LIMIT"
+    exit 1
+fi
+
+# Check if the limit RSS is provided
+if [ -z "$2" ]; then
+    echo "Usage: $0 PID INTERVALL RSS_LIMIT"
+    exit 1
+fi
+
+# Check if the Interval (in s) is provided
+if [ -z "$3" ]; then
+    echo "Usage: $0 PID INTERVALL RSS_LIMIT"
     exit 1
 fi
 
@@ -16,32 +28,34 @@ fi
 INTERVAL=$2
 RSS_limit=$3
 
+sleep 20
+    
 while true; do
     echo "LastProcess PID= " $1
+    
+    ps
+    
+    # Get PID for the process "cmsRun" and the user "jenkins"
     p_all=$(ps -eo pid,user,comm | grep cmsRun | grep jenkins | awk '{print}')
-    echo $p_all
+    echo "=== > Information about the process (PID user name_process): " $p_all
+    
     PID=$(ps -eo pid,user,comm | grep cmsRun | grep jenkins | awk '{print $1}')
     echo "PID=" $PID
     
-    if [ -z "$PID" ]; then
+    if [ -z "$PID" ] || [ ! -e /proc/$PID/status ] ; then
         echo "Process $PID not found."
+        break;
     else
         # Get the RSS (Resident Set Size) memory usage
-        echo "Get RSS value "
         RSS=$(grep -i vmrss /proc/$PID/status | awk '{print $2}')
+        echo "Free memory (RSS) for process PID=$PID: ${RSS} kB"
+        
         if [ "${RSS}" -gt "${RSS_limit}" ]; then
             kill -9 $PID;
             echo "===> RSS memory ${RSS} > RSS limit ${RSS_limit}";
             break;
         fi
         
-        # Output the RSS memory usage
-        if [ -e /proc/$PID/status ]; then
-            echo "Free memory (RSS) for process $1 (PID: $PID): ${RSS} kB"
-        else
-            echo "ProduceData_multiconfiguration.py process finished. PID = $PID"
-            break;
-        fi
     fi
     
     sleep $INTERVAL
